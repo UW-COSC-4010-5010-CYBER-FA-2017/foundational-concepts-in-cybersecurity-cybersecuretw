f774ce9ee910148370cdd2a2d60af6476eda4fd4
Thomas Wilkinson <31521090+wilkitom@users.noreply.github.com>
2017-10-09 20:19:55 -0600

    Simplicity

---
+++ Concept9/src/SimplicityExample/SimplicityExample/Source.cpp
@@ -0,0 +1,13 @@
+//Thomas Wilkinson
+//10/4/2017
+//Cosc 4010 - CyberSecurity
+//#9 - Simplicity - example
+
+using namespace::std;
+#include <iostream>
+
+int main()
+{
+	cout << "Hello world!";
+	return 0;
+}
